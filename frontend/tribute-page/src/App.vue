0f52f6e50f815a6733a81e8b0e3e2c31bfb4e7b5
Amir Fakheraldeen <amir.f.aldeen@gmail.com>
2017-05-08 12:02:06 +0300

    Initialize Vue project for tribute-page

---
+++ frontend/tribute-page/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      <app-header></app-header>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+    name: "app",
+    components: {
+        "app-header": Header
+    }
+};
+</script>
+
+<style>
+@import "assets/scss/style.scss";
+</style>
---
+++ frontend/tribute-page/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <header>
+        <h1>Sony PlayStation</h1>
+        <p>One of the leading video game consoles</p>
+    </header>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
