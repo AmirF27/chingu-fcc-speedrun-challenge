f22b9b5cfaf271cefb635111ebbeb5a6bfb03efb
Amir Fakheraldeen <amir.f.aldeen@gmail.com>
2017-05-13 21:18:31 +0300

    Add ability to show all, online, or offline users
    Added ability to toggle between showing all, online, or offline users
    to the Twitch.tv client project.

--- frontend/twitch-client/src/components/Main.vue
+++ frontend/twitch-client/src/components/Main.vue
@@ -1,7 +1,12 @@
 <template>
     <main class="container">
+        <div class="center-text">
+            <button @click="showAll" class="btn btn--default">All</button>
+            <button @click="showOnline" class="btn btn--default">Online</button>
+            <button @click="showOffline" class="btn btn--default">Offline</button>
+        </div>
         <ul v-if="ready" class="list">
-            <li v-for="user in userData" class="list-item">
+            <li v-for="user in displayedUsers" class="list-item">
                 <img :src="user.logo" :alt="user.displayName">
                 {{ user.displayName }}
                 <span class="right">
@@ -12,7 +17,9 @@
                 </p>
             </li>
         </ul>
-        <img v-else class="center-text" src="../assets/img/ajax-loader.gif" alt="Loading">
+        <div v-else class="center-text">
+            <img src="../assets/img/ajax-loader.gif" alt="Loading">
+        </div>
     </main>
 </template>
 
@@ -33,12 +40,25 @@ export default {
                 "noobs2ninjas"
             ],
             userData: [],
+            displayedUsers: [],
             ready: false
         };
     },
+    methods: {
+        showAll() {
+            this.displayedUsers = this.userData.slice();
+        },
+        showOnline() {
+            this.displayedUsers = this.userData.filter(user => user.online);
+        },
+        showOffline() {
+            this.displayedUsers = this.userData.filter(user => !user.online);
+        }
+    },
     created() {
         this.ready = false;
-        var vm = this;
+        var vm = this,
+            requestsDone = 0;
 
         for (let user of this.users) {
             let data = {};
@@ -53,14 +73,14 @@ export default {
             .then(function fulfilled(stream) {
                 Object.assign(data, stream);
                 vm.userData.push(data);
+                // check if we're done getting the data
+                if (++requestsDone === vm.users.length) {
+                    vm.ready = true;
+                    vm.userData.sort((a, b) => b.online - a.online);
+                    vm.showAll();
+                }
             });
         }
-
-        this.userData.sort(function(a, b) {
-            return a.online - b.online;
-        });
-
-        this.ready = true;
     }
 };
 </script>
