35d94ddfab8d8134c2ecedb7a2d579581d3792d5
Amir Fakheraldeen <amir.f.aldeen@gmail.com>
2017-05-14 14:35:17 +0300

    Set up JavaScript calculator project

---
+++ frontend/calculator/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="app">
+        <app-header :projectTitle="projectTitle"></app-header>
+        <app-footer :cert="cert" :project="project"></app-footer>
+    </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+const PROJECT_TITLE = "JavaScript Calculator";
+const CERT = "frontend";
+const PROJECT = "calculator";
+
+export default {
+  name: "app",
+  components: {
+      "app-header": Header,
+      "app-footer": Footer,
+  },
+  data() {
+      return {
+          projectTitle: PROJECT_TITLE,
+          cert: CERT,
+          project: PROJECT
+      };
+  }
+};
+</script>
+
+<style>
+@import "./assets/scss/style.scss";
+</style>
