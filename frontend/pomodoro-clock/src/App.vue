74d07bced1739a78d134747feaa3ea2fdc035eac
Amir Fakheraldeen <amir.f.aldeen@gmail.com>
2017-05-17 02:36:30 +0300

    Set up Pomodoro Clock project

---
+++ frontend/pomodoro-clock/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="app">
+        <app-header :projectTitle="projectTitle"></app-header>
+        <app-main></app-main>
+        <app-footer :cert="cert" :project="project"></app-footer>
+    </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+
+const PROJECT_TITLE = "Pomodoro Clock";
+const CERT = "frontend";
+const PROJECT = "pomodoro-clock";
+
+export default {
+    name: "app",
+    components: {
+        "app-header": Header,
+        "app-main": Main,
+        "app-footer": Footer,
+    },
+    data() {
+        return {
+            projectTitle: PROJECT_TITLE,
+            cert: CERT,
+            project: PROJECT
+        };
+    }
+};
+</script>
+
+<style>
+@import "./assets/scss/style";
+</style>
---
+++ frontend/pomodoro-clock/src/components/Main.vue
@@ -0,0 +1,7 @@
+<template>
+    <main class="container"></main>
+</template>
+
+<script>
+export default {};
+</script>
