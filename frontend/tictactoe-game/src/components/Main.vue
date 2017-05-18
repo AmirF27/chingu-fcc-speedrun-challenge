f0bb44897da602bd2cd8015b3f8c7e8ddabda8f1
Amir Fakheraldeen <amir.f.aldeen@gmail.com>
2017-05-18 14:43:52 +0300

    Add basic functionality to Tic Tac Toe project

--- frontend/tictactoe-game/src/components/Main.vue
+++ frontend/tictactoe-game/src/components/Main.vue
@@ -1,6 +1,57 @@
 <template>
-    <main class="container"></main>
+    <main class="container">
+        <div class="board">
+            <template v-for="(row, rowIndex) in board">
+                <div v-for="(square, index) in row"
+                    @click="fillSquare(rowIndex, index)">
+                    {{ square }}
+                </div>
+            </template>
+        </div>
+    </main>
 </template>
 
 <script>
+export default {
+    data() {
+        return {
+            board: [
+                ["", "", ""],
+                ["", "", ""],
+                ["", "", ""]
+            ],
+            counter: 0
+        };
+    },
+    methods: {
+        fillSquare(row, index) {
+            this.$set(this.board[row], index, "X");
+
+            this.counter++;
+
+            if (this.counter < 9) {
+                let square;
+
+                do {
+                    square = this.randomSquare();
+                } while (this.board[square.x][square.y]);
+
+                this.$set(this.board[square.x], square.y, "O");
+                this.counter++;
+            }
+
+            console.log(this.counter);
+        },
+        randomSquare() {
+            var square = {};
+
+            Object.assign(square, {
+                x: Math.floor(Math.random() * this.board.length),
+                y: Math.floor(Math.random() * this.board.length)
+            });
+
+            return square;
+        }
+    }
+};
 </script>
